edc26482991ef51a3968d68c66cbc67b72ba1b8f
AnotherTwinkle <anothersparklingstar@gmail.com>
2022-02-04 11:51:28 +0600

    Implement flags

--- cftool/commands.py
+++ cftool/commands.py
@@ -3,13 +3,13 @@ class Parser:
         self._commands = {}
         self.alias_table = {}
 
-    def parse(self, command, arguments):
+    def parse(self, command, arguments, flags):
         name = self.alias_table.get(command, None)
         if name is None:
             raise CommandNotFound
 
         command = self._commands.get(name, None)
-        return command._parse_and_invoke(arguments)
+        return command._parse_and_invoke(arguments, flags)
 
     def command(self, parser_func, **kwargs):
         def decorator(func):
@@ -33,6 +33,28 @@ class Parser:
             return command 
         return decorator
 
+    def parse_flags(self, args):
+        flags, notflags = ({}, [])
+        x = 0
+        while x < len(args):
+            arg = args[x]
+            if arg.startswith('--') and len(arg) > 2:
+                flags[arg[2:]] = True
+            elif arg.startswith('-') and len(arg) > 1:
+                try:
+                    if args[x+1].startswith('-'): # Store flags. These are currently unused.
+                        raise Exception(f'No value was provided for flag {arg}')
+                    flags[arg[1:]] = args[x+1] # The next argument should be the value
+
+                except IndexError:
+                    raise Exception(f'Unexpected end of input after flag declaration.')
+                x += 1 # We will skip the next index
+            else:
+                notflags.append(arg)
+            x += 1
+
+        return flags, notflags
+
     @property
     def commands(self):
         return [command for command in self._commands.values()]
@@ -46,10 +68,26 @@ class Command:
 
         self.callback = func
         self.parser_func = parser_func
+        self.params = self.callback.__code__.co_varnames[:self.callback.__code__.co_argcount]
+        self.flags = kwargs.get('flags', [])
 
-    def _parse_and_invoke(self, arguments):
+        for flag in self.flags:
+            if flag not in self.params:
+                raise Exception(f'Flag "{flag}" is not an argument in function {self.callback}')
+
+    def _parse_and_invoke(self, arguments, flags):
         args = self.parser_func(arguments)
-        return self.callback(*args)
+        
+        requiredflags = [flag for flag in flags if flag in self.flags] 
+        ignoredflags = [flag for flag in flags if flag not in requiredflags]
+
+        for flag in ignoredflags:
+            print(f'Ignoring unexpected flag: "{flag}"')
+
+        for flag in requiredflags:
+            args[flag] = flags[flag]
+
+        return self(**args)
 
     def __call__(self, *args, **kwargs):
         return self.callback(*args, **kwargs)
